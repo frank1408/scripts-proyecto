ef09611ff33aed709ce53d1b3056124c45cd9d3c
Franklin Rodriguez <frank14082013@gmail.com>
2022-10-04 22:32:31 -0700

    nuevos detalles de grupos con cgroup

--- limites.sh
+++ limites.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
 RUTA="/sys/fs/cgroup/init.scope"
 
+/sys/fs/cgroup/user.slice
+/pids.max
+/memory.swap.max
+/memory.max
+/cpu.max
+
+
+/sys/fs/cgroup/system.slice
+echo "+cpu" > cgroup.subtree_control
+/pids.max
+/memory.swap.max
+/memory.max
+/cpu.max
+
+
+
+
+
+
 cd $RUTA
+echo "+cpu" > cgroup.subtree_control
 cd ..
 echo "+cpu" > cgroup.subtree_control
 cd $RUTA
